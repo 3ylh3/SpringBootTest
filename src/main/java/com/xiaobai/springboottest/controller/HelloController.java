5d2dfd1adcc0efacdbe1e22349dc95df4453bbe8
3ylh3 <781706889@qq.com>
2019-01-09 17:25:38 +0800

    modify

--- src/main/java/com/xiaobai/springboottest/controller/HelloController.java
+++ src/main/java/com/xiaobai/springboottest/controller/HelloController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 public class HelloController {
     @RequestMapping(value = "/hello",method = RequestMethod.POST)
